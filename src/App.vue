149ebb33ce7792383c1aa30dc645f772d51b3022
floki <adilhan97kz@gmail.com>
2022-12-19 09:30:12 +0600

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
-    <canvas width="640" height="480" ref="canvas"/>
+    <canvas :width="widthScreen" :height="heightScreen" ref="canvas"/>
     <button class="connect" :disabled="connected" @click="connect">Connect</button>
     <input type="range" min="0" max="10" step="0.1" v-model="soundVolume"/>
+    <div class="flex">
+      <button @click="testGamepad(i)" v-for="i in 30" :key="i">{{i}}</button>
+    </div>
+    <div class="flex">
+      <button @click="testGamepad(30+i)" v-for="i in 30" :key="-i">{{30+i}}</button>
+    </div>
+    <div class="flex">
+      <button @click="testGamepad(i)" v-for="i in Object.values(CONTROLS)" :key="-i">{{i}}</button>
+    </div>
   </div>
 </template>
 
@@ -14,6 +23,8 @@ export default {
   name: 'App',
   data() {
     return {
+      widthScreen: 1280,
+      heightScreen: 1024,
       socket: null,
       context: null,
       canvas: null,
@@ -21,8 +32,22 @@ export default {
       soundShedTime: 0,
       audioContext: null,
       audioBuffer: null,
-      soundVolume: .5,
-      gamepadIndex: 1
+      soundVolume: 0,
+      gamepadIndex: 1,
+      CONTROLS: {
+        INPUT_MODE: 0x0800,
+        INPUT_X: 0x0400,
+        INPUT_Y: 0x0200,
+        INPUT_Z: 0x0100,
+        INPUT_START: 0x0080,
+        INPUT_A: 0x0040,
+        INPUT_C: 0x0020,
+        INPUT_B: 0x0010,
+        INPUT_RIGHT: 0x0008,
+        INPUT_LEFT: 0x0004,
+        INPUT_DOWN: 0x0002,
+        INPUT_UP: 0x0001
+      }
     }
   },
   mounted() {
@@ -46,8 +71,8 @@ export default {
     drawImage(data) {
       let image = new Image();
       image.onload = () => {
-        this.context.clearRect(0, 0, 640, 480)
-        this.context.drawImage(image, 0, 0);
+        this.context.clearRect(0, 0, this.widthScreen, this.heightScreen)
+        this.context.drawImage(image, 0, 0, this.widthScreen, this.heightScreen);
       };
       image.src = data;
     },
@@ -71,8 +96,8 @@ export default {
       this.audioContext = new (window.AudioContext || window.webkitAudioContext)({sampleRate: 44100});
       this.audioBuffer = this.audioContext.createBuffer(2, 736, 44100);
 
-      this.socket = io("http://51.250.77.85:3000")
-      // this.socket = io("ws://localhost:3000");
+      // this.socket = io("http://51.250.77.85:3000")
+      this.socket = io("ws://localhost:3000");
 
       this.socket.on('connect', () => {
         console.log('connected');
@@ -80,6 +105,14 @@ export default {
 
       this.socket.on('frame', (data) => {
         this.gamepadScan()
+
+
+        // if (width != lastWidth || height != lastHeight) {
+          // lastWidth = width;
+          // lastHeight = height;
+          // SetCanvasSize(lastWidth, lastHeight);
+        // }
+
         this.drawImage(data.image);
         this.audioBuffer.copyToChannel(new Float32Array(data.audio_l), 0);
         this.audioBuffer.copyToChannel(new Float32Array(data.audio_r), 1);
@@ -102,6 +135,13 @@ export default {
         this.soundShedTime = currentSoundTime + this.audioBuffer.duration + 736 * 8 / 44100;
       }
     },
+
+    testGamepad(index){
+      this.socket.emit('button', index, index)
+      setTimeout(()=>{
+        this.socket.emit('button', 0, index)
+      }, 200)
+    }
   }
 }
 </script>
